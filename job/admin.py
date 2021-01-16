20f263f4f8793db8032e728ab0da5d58945df505
Khaled Muhammad <khaledmuhmmed99@gmail.com>
2021-01-16 19:39:04 +0200

    Added db columns. and urls

--- job/admin.py
+++ job/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import job, job_category
+
+admin.site.register(job)
+admin.site.register(job_category)
--- job/models.py
+++ job/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+JTchoices = (
+    ("Full time job", "Full time job"),
+    ("Part time job", "Part time job")
+)
+
+class job(models.Model):
+    title = models.CharField(max_length=100)
+    #location
+    job_type = models.CharField(max_length=20, choices=JTchoices)
+    job_description = models.TextField(max_length=1000)
+    job_published_at = models.DateTimeField(auto_now=True)
+    job_vacancy = models.IntegerField(default=1)
+    job_salary = models.IntegerField(default=0)
+    job_category = models.ForeignKey('job_category', on_delete=models.CASCADE)
+    exprience = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.title
+
+class job_category(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
